cde84941607cc2a3bad9a8cd00e1faa02b982ef4
Dave Cridland <dave.cridland@surevine.com>
2015-11-05 16:09:50 +0000

    C++ initial implementation

---
+++ src/main/cxx/handle.h
@@ -0,0 +1,63 @@
+/*
+    jVamp
+
+    A Java host interface for Vamp audio analysis plugins
+
+    Centre for Digital Music, Queen Mary, University of London.
+    Copyright 2012 Chris Cannam and QMUL.
+  
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use, copy,
+    modify, merge, publish, distribute, sublicense, and/or sell copies
+    of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR
+    ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    Except as contained in this notice, the names of the Centre for
+    Digital Music; Queen Mary, University of London; and Chris Cannam
+    shall not be used in advertising or otherwise to promote the sale,
+    use or other dealings in this Software without prior written
+    authorization.
+
+    (
+    Copied from original source:
+    https://code.soundsoftware.ac.uk/projects/jvamp/repository/entry/src/handle.h
+    )
+*/
+
+#ifndef _HANDLE_H_INCLUDED_
+#define _HANDLE_H_INCLUDED_
+
+inline jfieldID getHandleField(JNIEnv *env, jobject obj)
+{
+    jclass c = env->GetObjectClass(obj);
+    return env->GetFieldID(c, "nativeHandle", "J");
+}
+
+template <typename T>
+T *getHandle(JNIEnv *env, jobject obj)
+{
+    jlong handle = env->GetLongField(obj, getHandleField(env, obj));
+    return reinterpret_cast<T *>(handle);
+}
+
+template <typename T>
+void setHandle(JNIEnv *env, jobject obj, T *t)
+{
+    jlong handle = reinterpret_cast<jlong>(t);
+    env->SetLongField(obj, getHandleField(env, obj), handle);
+}
+
+#endif
---
+++ src/main/cxx/site.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 Surevine Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * (These are the standard Surevine/MIT terms)
+ */
+
+#include "com_surevine_spiffing_Site.h"
+#include "handle.h"
+#include <spiffing/spiffing.h>
+#include <fstream>
+
+/*
+ * Class:     com_surevine_spiffing_Site
+ * Method:    init
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_com_surevine_spiffing_Site_init
+(JNIEnv * jenv, jobject jobj) {
+        setHandle(jenv, jobj, new Spiffing::Site());
+}
+
+/*
+ * Class:     com_surevine_spiffing_Site
+ * Method:    spif_native
+ * Signature: (Ljava/lang/String;)J
+ */
+JNIEXPORT jlong JNICALL Java_com_surevine_spiffing_Site_spif_1native
+        (JNIEnv * jenv, jobject jobj, jstring joid) {
+        auto deleter = [&joid, &jenv](const char * d) {
+            jenv->ReleaseStringUTFChars(joid, d);
+        };
+        std::unique_ptr<const char, decltype(deleter)> oid(jenv->GetStringUTFChars(joid, nullptr), deleter);
+        std::shared_ptr<Spiffing::Spif> spif(Spiffing::Site::site().spif(&*oid));
+        return reinterpret_cast<jlong>(&*spif);
+}
+
+/*
+ * Class:     com_surevine_spiffing_Site
+ * Method:    load_native
+ * Signature: (Ljava/lang/String;)J
+ */
+JNIEXPORT jlong JNICALL Java_com_surevine_spiffing_Site_load_1native
+        (JNIEnv * jenv, jobject jobj, jstring jfilename) {
+        auto deleter = [&jfilename, &jenv](const char * d) {
+            jenv->ReleaseStringUTFChars(jfilename, d);
+        };
+        std::unique_ptr<const char, decltype(deleter)> filename(jenv->GetStringUTFChars(jfilename, nullptr), deleter);
+        std::ifstream ifs(&*filename);
+        std::shared_ptr<Spiffing::Spif> spif(Spiffing::Site::site().load(ifs));
+        return reinterpret_cast<jlong>(&*spif);
+}
+
+/*
+ * Class:     com_surevine_spiffing_Site
+ * Method:    dispose_native
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_com_surevine_spiffing_Site_dispose_1native
+(JNIEnv * jenv, jobject jobj) {
+        Spiffing::Site * s = getHandle<Spiffing::Site>(jenv, jobj);
+        delete s;
+        setHandle<Spiffing::Site>(jenv, jobj, nullptr);
+}
